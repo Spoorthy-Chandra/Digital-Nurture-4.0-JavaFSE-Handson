3829db0388630f5fd1516fd6839202c103888275
Spoorthy-Chandra <151895809+Spoorthy-Chandra@users.noreply.github.com>
2025-06-18 11:51:36 +0530

    Create LoggerTest.java

---
+++ WEEK 1/Singleton Pattern/LoggerTest.java
@@ -0,0 +1,18 @@
+public class LoggerTest {
+    public static void main(String[] args) {
+       
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+       
+        logger1.log("First message");
+        logger2.log("Second message");
+
+      
+        if (logger1 == logger2) {
+            System.out.println("Both logger instances are the same (Singleton works).");
+        } else {
+            System.out.println("Logger instances are different (Singleton failed).");
+        }
+    }
+}
